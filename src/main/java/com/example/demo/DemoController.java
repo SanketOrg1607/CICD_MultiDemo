a47139bc624d474fa74f47e6da7dc814c766aa73
sanket <sanketkhokade50@gmail.com>
2026-08-22 12:56:22 +0530

    Adding files

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello World - DevOps CI/CD Demo";
+    }
+}
