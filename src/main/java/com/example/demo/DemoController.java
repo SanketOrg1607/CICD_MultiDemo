3a7fe7317ed6b876e54c079737b71e31f4181620
sanketkhokade50-hue <sanketkhokade50@gmail.com>
2026-08-24 18:07:36 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/")
     public String welcome() {
-        return "Hello World - DevOps CI/CD Demo";
+        return "Hello World - I am Sanket From Sangli";
     }
 }
